94a964dd55a732b6496d8eff7d52d052ce16d92a
Sebastian Florez <sebasflorez69@gmail.com>
2024-12-10 12:33:55 -0500

    first commit

---
+++ src/main/java/app/PetShere/PetShereApplication.java
@@ -0,0 +1,13 @@
+package app.PetShere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetShereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/PetShere/PetShereApplicationTests.java
@@ -0,0 +1,13 @@
+package app.PetShere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
