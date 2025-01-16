8fe2dfb0b8d132e90be8ab0626bba996d86af39b
Sebastian Florez <sebasflorez69@gmail.com>
2025-01-16 13:54:55 -0500

    feat: autorización de métodos implementada

--- src/main/java/app/PetShere/controllers/DemoController.java
+++ src/main/java/app/PetShere/controllers/DemoController.java
@@ -1,10 +1,13 @@
 package app.PetShere.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/demo")
 @RequiredArgsConstructor
@@ -13,5 +16,15 @@ public class DemoController {
     @PostMapping
     public String welcome() {
         return "Authentication works!";
+    }
+
+    // NOTE: Use PreAuthorize for each method that needs authorization
+    @GetMapping("/admin")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public String testOnlyAdminRequest() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String roles = authentication.getAuthorities().toString();
+        log.warn("Rol: " + roles);
+        return "Admin authorized";
     }
 }
