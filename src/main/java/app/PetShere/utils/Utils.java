eefb041599314fdfe8980c0eb426592d8a3f1694
Sebastian Florez <sebasflorez69@gmail.com>
2025-01-25 23:40:58 -0500

    refactor: cambio en clases varias

--- src/main/java/app/PetShere/utils/Utils.java
+++ src/main/java/app/PetShere/utils/Utils.java
@@ -1,8 +1,24 @@
 package app.PetShere.utils;
 
-import java.util.Date;
+import app.PetShere.models.user.User;
+import app.PetShere.repositories.user.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
+import java.util.Date;
+import java.util.Optional;
+
+@Component
 public class Utils {
+
+    public static Optional<User> getCurrentUser(UserRepository userRepository) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        return userRepository.findByEmail(
+                authentication.getPrincipal().toString()
+        );
+    }
 
     public static Date tenMinutesFromNow() {
         return new Date(System.currentTimeMillis() + minutesToMiliseconds(10));
