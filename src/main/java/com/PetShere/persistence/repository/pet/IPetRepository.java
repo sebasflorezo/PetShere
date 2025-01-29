471d754e9c22e1a4831db0616ea9a71f98e4a6ff
Sebastian Florez <sebasflorez69@gmail.com>
2025-01-29 17:59:20 -0500

    feat: modelo y repositorio de historial médico agregado

--- src/main/java/com/PetShere/persistence/repository/pet/IPetRepository.java
+++ src/main/java/com/PetShere/persistence/repository/pet/IPetRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface IPetRespository extends JpaRepository<Pet, Long> {
+public interface IPetRepository extends JpaRepository<Pet, Long> {
     List<Pet> findByOwnerDocument(String ownerDocument);
 
     Boolean existsByIdAndOwnerDocument(Long id, String document);
