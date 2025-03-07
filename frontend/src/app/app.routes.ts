b8ce9012dca5456a35436c13757a51e29474c998
Sebastian Florez <sebasflorez69@gmail.com>
2025-03-06 19:05:32 -0500

    fix: header y footer solo páginas estáticas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ServicesComponent } from './pages/services/services.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'services', component: ServicesComponent },
+    { path: 'login', component: LoginComponent, pathMatch: "full" },
 ];
--- frontend/src/app/pages/about/about.component.ts
+++ frontend/src/app/pages/about/about.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
