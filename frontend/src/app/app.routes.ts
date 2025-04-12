b83617e5a12f38f214ed9517bbfdb24e5ee88493
Sebastian Florez <sebasflorez69@gmail.com>
2025-04-12 13:57:22 -0500

    fix: arreglo de peticiones por proxy

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,13 +5,17 @@ import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { ServicesComponent } from './pages/services/services.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'about', component: AboutComponent },
-    { path: 'contact', component: ContactComponent },
-    { path: 'services', component: ServicesComponent },
-    { path: 'login', component: LoginComponent, pathMatch: "full" },
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent, pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent, pathMatch: 'full' },
 
-    { path: '**', component: NotFoundComponent }
+  { path: '**', component: NotFoundComponent },
 ];
